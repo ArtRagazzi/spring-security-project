1747e1d2d0e67facadf0b02d53bd8f3a9b91368e
artur oliviera <ao.ragazzi@gmail.com>
2025-03-20 15:43:22 -0300

    Configurando credenciais de acesso

--- src/main/java/com/artragazzi/spring_security_api/config/SecurityConfig.java
+++ src/main/java/com/artragazzi/spring_security_api/config/SecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,6 +28,29 @@ public class SecurityConfig {
 
                 .build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails comumUser = User.builder()
+                .username("artur")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("321"))
+                .roles("ADMIN", "USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(comumUser,adminUser);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
 
